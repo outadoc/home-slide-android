8f7f6f95fa54d332e363edae861afa75255dbe53
Baptiste Candellier <outadoc@gmail.com>
2019-06-25 19:12:39 +0200

    Make state immutable

--- app/src/main/java/fr/outadoc/quickhass/model/Entity.kt
+++ app/src/main/java/fr/outadoc/quickhass/model/Entity.kt
@@ -4,24 +4,31 @@ import fr.outadoc.quickhass.model.annotation.StringDomain
 import fr.outadoc.quickhass.model.annotation.StringEntityId
 import fr.outadoc.quickhass.model.annotation.StringState
 
-class Entity(private var state: State) {
+class Entity(state: State) {
 
     @StringEntityId
-    val entityId: String
-        get() = state.entityId
+    val entityId: String = state.entityId
 
     @StringState
-    val stateStr: String
-        get() = state.state
-
-    val attributes: AttributeSet
-        get() = state.attributes
+    val stateStr: String = state.state
 
     @StringDomain
-    val domain: String
-        get() = entityId.takeWhile { it != '.' }
+    val domain: String = entityId.takeWhile { it != '.' }
 
-    fun updateState(state: State) {
-        this.state = state
+    val attributes: AttributeSet = state.attributes
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Entity
+
+        if (entityId != other.entityId) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return entityId.hashCode()
     }
 }
