974265edd6bdb6d25d12dedd7e6a9ad131c78f4e
Baptiste Candellier <outadoc@gmail.com>
2019-07-01 21:54:19 +0200

    Replace Timer with a ScheduledThreadPoolExecutor

--- app/src/main/java/fr/outadoc/quickhass/slideover/EntityGridViewModel.kt
+++ app/src/main/java/fr/outadoc/quickhass/slideover/EntityGridViewModel.kt
@@ -13,8 +13,9 @@ import fr.outadoc.quickhass.rest.HomeAssistantServerImpl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
-import java.util.*
-import kotlin.concurrent.schedule
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
 
 
 class EntityGridViewModel(application: Application) : AndroidViewModel(application) {
@@ -34,10 +35,9 @@ class EntityGridViewModel(application: Application) : AndroidViewModel(applicati
     private val _shouldAskForInitialValues = MutableLiveData<Boolean>()
     val shouldAskForInitialValues: LiveData<Boolean> = _shouldAskForInitialValues
 
-    private val timer = Timer("Periodic refresh", false)
+    private var threadPoolExecutor: ScheduledExecutorService? = null
 
     fun loadShortcuts() {
-        println(prefs.shouldAskForInitialValues)
         if (prefs.shouldAskForInitialValues) {
             _shouldAskForInitialValues.value = prefs.shouldAskForInitialValues
             return
@@ -66,10 +66,19 @@ class EntityGridViewModel(application: Application) : AndroidViewModel(applicati
                 _isLoading.postValue(false)
             }
 
-            timer.schedule(REFRESH_INTERVAL) {
-                loadShortcuts()
-            }
+            scheduleRefresh()
         }
+    }
+
+    private fun scheduleRefresh() {
+        val executor = threadPoolExecutor
+        if (executor == null || executor.isShutdown) {
+            threadPoolExecutor = Executors.newSingleThreadScheduledExecutor()
+        }
+
+        threadPoolExecutor?.schedule({
+            loadShortcuts()
+        }, REFRESH_INTERVAL_S, TimeUnit.SECONDS)
     }
 
     fun onEntityClick(item: Entity) {
@@ -99,11 +108,12 @@ class EntityGridViewModel(application: Application) : AndroidViewModel(applicati
 
     override fun onCleared() {
         super.onCleared()
-        timer.cancel()
+        threadPoolExecutor?.shutdown()
+        threadPoolExecutor = null
     }
 
     companion object {
-        const val REFRESH_INTERVAL = 10000L
+        const val REFRESH_INTERVAL_S = 10L
 
         val INITIAL_DOMAIN_BLACKLIST = listOf(
             "automation",
