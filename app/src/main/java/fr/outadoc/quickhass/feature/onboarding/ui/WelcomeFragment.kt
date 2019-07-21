0b5b6e2b476b11c560cf0a2e065cda3fe33e3292
Baptiste Candellier <outadoc@gmail.com>
2019-07-21 20:15:50 +0200

    Finish onboarding flow

--- app/src/main/java/fr/outadoc/quickhass/feature/onboarding/ui/WelcomeFragment.kt
+++ app/src/main/java/fr/outadoc/quickhass/feature/onboarding/ui/WelcomeFragment.kt
@@ -6,20 +6,38 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import fr.outadoc.quickhass.R
+import fr.outadoc.quickhass.feature.onboarding.model.NavigationFlow
+import fr.outadoc.quickhass.feature.onboarding.vm.WelcomeViewModel
 
 class WelcomeFragment : Fragment() {
 
+    private lateinit var viewModel: WelcomeViewModel
     private lateinit var viewHolder: ViewHolder
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProviders.of(this).get(WelcomeViewModel::class.java).apply {
+            navigateTo.observe(this@WelcomeFragment, Observer {
+                when (it.pop()) {
+                    NavigationFlow.NEXT -> viewHolder.navController.navigate(R.id.action_welcomeFragment_to_setupHostFragment)
+                    else -> viewHolder.navController.navigateUp()
+                }
+            })
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_welcome, container, false)
 
         viewHolder = ViewHolder(view).apply {
             continueButton.setOnClickListener {
-                navController.navigate(R.id.action_welcomeFragment_to_setupHostFragment)
+                viewModel.onContinueClicked()
             }
         }
 
--- app/src/main/java/fr/outadoc/quickhass/feature/onboarding/vm/SuccessViewModel.kt
+++ app/src/main/java/fr/outadoc/quickhass/feature/onboarding/vm/SuccessViewModel.kt
@@ -1,17 +1,22 @@
 package fr.outadoc.quickhass.feature.onboarding.vm
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import fr.outadoc.quickhass.feature.onboarding.model.NavigationFlow
 import fr.outadoc.quickhass.lifecycle.Event
+import fr.outadoc.quickhass.preferences.PreferenceRepositoryImpl
 
-class WelcomeViewModel : ViewModel() {
+class SuccessViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val prefs = PreferenceRepositoryImpl(application.applicationContext)
 
     private val _navigateTo = MutableLiveData<Event<NavigationFlow>>()
     val navigateTo: LiveData<Event<NavigationFlow>> = _navigateTo
 
     fun onContinueClicked() {
+        prefs.isOnboardingDone = true
         _navigateTo.value = Event(NavigationFlow.NEXT)
     }
 }
