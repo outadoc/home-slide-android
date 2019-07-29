175b6883fcf1bc76710c6930e7aae5ddfb70bccb
Baptiste Candellier <outadoc@gmail.com>
2019-07-29 19:00:17 +0200

    Add confetti to the success fragment 🎉

--- app/src/main/java/fr/outadoc/quickhass/feature/onboarding/ui/SuccessFragment.kt
+++ app/src/main/java/fr/outadoc/quickhass/feature/onboarding/ui/SuccessFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.view.doOnNextLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -13,11 +14,16 @@ import androidx.navigation.findNavController
 import fr.outadoc.quickhass.R
 import fr.outadoc.quickhass.feature.onboarding.model.NavigationFlow
 import fr.outadoc.quickhass.feature.onboarding.vm.SuccessViewModel
+import nl.dionsegijn.konfetti.KonfettiView
+import nl.dionsegijn.konfetti.models.Shape
+import nl.dionsegijn.konfetti.models.Size
 
 class SuccessFragment : Fragment() {
 
     private lateinit var viewModel: SuccessViewModel
     private lateinit var viewHolder: ViewHolder
+
+    private val confettiColors = intArrayOf(R.color.lt_yellow, R.color.lt_orange, R.color.lt_purple, R.color.lt_pink)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,25 +43,38 @@ class SuccessFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_success, container, false)
 
         viewHolder = ViewHolder(view).apply {
             continueButton.setOnClickListener {
                 viewModel.onContinueClicked()
             }
+
+            confettiView.doOnNextLayout { confetti() }
         }
 
         return view
+    }
 
+    private fun confetti() {
+        viewHolder.confettiView.apply {
+            build()
+                .addColors(confettiColors.map { context.getColor(it) })
+                .setDirection(0.0, 359.0)
+                .setSpeed(4f, 7f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(2000L)
+                .addShapes(Shape.RECT, Shape.CIRCLE)
+                .addSizes(Size(12), Size(16, 6f))
+                .setPosition(-50f, width + 50f, -50f, -50f)
+                .streamFor(300, 2000L)
+        }
     }
 
     private class ViewHolder(private val view: View) {
         val continueButton: Button = view.findViewById(R.id.btn_continue)
+        val confettiView: KonfettiView = view.findViewById(R.id.konfetti)
 
         val navController: NavController
             get() = view.findNavController()
