f685305ff0cd63e24f642b945c048c2c2a0c673c
Baptiste Candellier <outadoc@gmail.com>
2019-08-22 21:49:01 +0200

    Add version in settings

--- app/src/main/java/fr/outadoc/quickhass/preferences/AppPreferencesFragment.kt
+++ app/src/main/java/fr/outadoc/quickhass/preferences/AppPreferencesFragment.kt
@@ -1,6 +1,8 @@
 package fr.outadoc.quickhass.preferences
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import fr.outadoc.quickhass.R
 
@@ -9,6 +11,16 @@ class AppPreferencesFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences_main, rootKey)
+
+        findPreference<Preference>("pref_about_version")?.apply {
+            try {
+                val pInfo = context.packageManager.getPackageInfo(context.packageName, 0)
+                summary = getString(R.string.version_name, pInfo.versionName)
+            } catch (e: PackageManager.NameNotFoundException) {
+                e.printStackTrace()
+                summary = null
+            }
+        }
     }
 
     companion object {
