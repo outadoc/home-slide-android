b4aee9275490f41669626f1022b72f0adff473a0
Baptiste Candellier <outadoc@gmail.com>
2019-08-23 19:48:57 +0200

    Add credits to third-party software

--- app/src/main/java/fr/outadoc/quickhass/preferences/AppPreferencesFragment.kt
+++ app/src/main/java/fr/outadoc/quickhass/preferences/AppPreferencesFragment.kt
@@ -9,17 +9,31 @@ import fr.outadoc.quickhass.R
 
 class AppPreferencesFragment : PreferenceFragmentCompat() {
 
+    private val licenses = mapOf(
+        "mit" to R.array.pref_oss_mit_summary,
+        "apache2" to R.array.pref_oss_apache2_summary,
+        "isc" to R.array.pref_oss_isc_summary,
+        "ofl" to R.array.pref_oss_ofl_summary
+    )
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences_main, rootKey)
 
         findPreference<Preference>("pref_about_version")?.apply {
-            try {
+            summary = try {
                 val pInfo = context.packageManager.getPackageInfo(context.packageName, 0)
-                summary = getString(R.string.version_name, pInfo.versionName)
+                getString(R.string.version_name, pInfo.versionName)
             } catch (e: PackageManager.NameNotFoundException) {
                 e.printStackTrace()
-                summary = null
+                null
             }
+        }
+
+        licenses.forEach { (license, content) ->
+            findPreference<Preference>("pref_oss_$license")?.summary =
+                resources
+                    .getStringArray(content)
+                    .joinToString(separator = "\n")
         }
     }
 
