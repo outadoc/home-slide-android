e9655757681d699c7e97871436d583bfb5c9fe0d
Baptiste Candellier <outadoc@gmail.com>
2019-08-24 16:37:42 +0200

    Add icon credits

--- app/src/main/java/fr/outadoc/quickhass/preferences/AppPreferencesFragment.kt
+++ app/src/main/java/fr/outadoc/quickhass/preferences/AppPreferencesFragment.kt
@@ -13,7 +13,8 @@ class AppPreferencesFragment : PreferenceFragmentCompat() {
         "mit" to R.array.pref_oss_mit_summary,
         "apache2" to R.array.pref_oss_apache2_summary,
         "isc" to R.array.pref_oss_isc_summary,
-        "ofl" to R.array.pref_oss_ofl_summary
+        "ofl" to R.array.pref_oss_ofl_summary,
+        "ccby" to R.array.pref_oss_ccby_summary
     )
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
