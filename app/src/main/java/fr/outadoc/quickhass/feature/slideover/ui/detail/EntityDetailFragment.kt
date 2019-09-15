f5af2f3a76380057ba5bab667780b8561a0a4a6d
Baptiste Candellier <outadoc@gmail.com>
2019-09-15 19:37:33 +0200

    Display title in entity details

--- app/src/main/java/fr/outadoc/quickhass/feature/slideover/ui/detail/EntityDetailFragment.kt
+++ app/src/main/java/fr/outadoc/quickhass/feature/slideover/ui/detail/EntityDetailFragment.kt
@@ -4,22 +4,43 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import fr.outadoc.quickhass.R
 import fr.outadoc.quickhass.feature.slideover.model.EntityState
 import fr.outadoc.quickhass.feature.slideover.model.entity.Entity
+import fr.outadoc.quickhass.feature.slideover.model.entity.EntityFactory
 import fr.outadoc.quickhass.feature.slideover.model.entity.LightEntity
+import kotlin.properties.Delegates
 
 class EntityDetailFragment private constructor() : Fragment() {
 
+    private var viewHolder: ViewHolder? = null
+    private var entity: Entity? by Delegates.observable(null) { _, _: Entity?, newValue: Entity? ->
+        newValue?.let {
+            viewHolder?.entityName?.text = it.friendlyName
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_entity_detail_container, container, false)
+        val root = inflater.inflate(R.layout.fragment_entity_detail_container, container, false)
+        viewHolder = ViewHolder(root).apply {
+            backButton.setOnClickListener {
+                activity?.onBackPressed()
+            }
+
+            entityName.text = entity?.friendlyName
+        }
+        return root
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         arguments?.getParcelable<EntityState>(ARGS_STATE)?.let { state ->
+            entity = EntityFactory.create(state) as? LightEntity
+
             childFragmentManager
                 .beginTransaction()
                 .replace(R.id.frameLayout_detailsContent, getChildFragment(state))
@@ -32,6 +53,16 @@ class EntityDetailFragment private constructor() : Fragment() {
             LightEntity.DOMAIN -> LightEntityDetailFragment.newInstance(state)
             else -> throw IllegalArgumentException("No detail fragment for ${state.domain}")
         }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewHolder = null
+    }
+
+    private class ViewHolder(view: View) {
+        val backButton: ImageButton = view.findViewById(R.id.imageButton_back)
+        val entityName: TextView = view.findViewById(R.id.textView_entityName)
+    }
 
     companion object {
 
