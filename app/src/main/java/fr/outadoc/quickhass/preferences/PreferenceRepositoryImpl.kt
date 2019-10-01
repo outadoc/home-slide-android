723ec6e36ddad80f5322e69eb8fcecda6ce5f33f
Baptiste Candellier <outadoc@gmail.com>
2019-10-01 23:22:10 +0200

    Remember last base url used

--- app/src/main/java/fr/outadoc/quickhass/preferences/PreferenceRepositoryImpl.kt
+++ app/src/main/java/fr/outadoc/quickhass/preferences/PreferenceRepositoryImpl.kt
@@ -3,7 +3,6 @@ package fr.outadoc.quickhass.preferences
 import android.content.Context
 import android.preference.PreferenceManager
 
-
 class PreferenceRepositoryImpl(context: Context) : PreferenceRepository {
 
     private var appPrefs = PreferenceManager.getDefaultSharedPreferences(context)!!
@@ -23,6 +22,15 @@ class PreferenceRepositoryImpl(context: Context) : PreferenceRepository {
             appPrefs.edit().putString(KEY_ACCESS_TOKEN, value).apply()
         }
 
+    override var preferredBaseUrl: PreferredBaseUrl
+        get() {
+            val value = appPrefs.getString(KEY_PREFERRED_BASE_URL, PreferredBaseUrl.PRIMARY.id)!!
+            return PreferredBaseUrl.values().first { it.id == value }
+        }
+        set(value) {
+            appPrefs.edit().putString(KEY_PREFERRED_BASE_URL, value.id).apply()
+        }
+
     override val theme: String
         get() = appPrefs.getString(KEY_THEME, "system")!!
 
@@ -38,6 +46,7 @@ class PreferenceRepositoryImpl(context: Context) : PreferenceRepository {
     companion object {
         const val KEY_INSTANCE_BASE_URL = "et_pref_instance_base_url"
         const val KEY_INSTANCE_ALT_BASE_URL = "et_pref_instance_alt_base_url"
+        const val KEY_PREFERRED_BASE_URL = "enum_pref_preferred_base_url"
         const val KEY_ACCESS_TOKEN = "et_pref_auth_token"
         const val KEY_THEME = "list_pref_theme"
         const val KEY_IS_ONBOARDING_DONE = "chk_pref_onboarding_ok"
