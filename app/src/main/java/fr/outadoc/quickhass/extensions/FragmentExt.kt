c019bdc80ab511788e1a32aaf69e994ec535cdb3
Baptiste Candellier <outadoc@gmail.com>
2019-10-08 23:18:36 +0200

    Move app preferences to slideover fragment

---
+++ app/src/main/java/fr/outadoc/quickhass/extensions/FragmentExt.kt
@@ -0,0 +1,15 @@
+package fr.outadoc.quickhass.extensions
+
+import androidx.annotation.StringRes
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+private val Fragment.supportActionBar: ActionBar?
+    get() = (activity as? AppCompatActivity)?.supportActionBar
+
+fun Fragment.setupToolbar(@StringRes title: Int, canGoBack: Boolean) {
+    supportActionBar?.setTitle(title)
+    supportActionBar?.setDisplayHomeAsUpEnabled(canGoBack)
+    setHasOptionsMenu(true)
+}
