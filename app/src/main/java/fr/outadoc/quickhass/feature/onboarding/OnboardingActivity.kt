dd9084889b8f066da42207cbe1848c4cbe80a003
Baptiste Candellier <outadoc@gmail.com>
2019-11-06 20:29:48 +0100

    Make onboarding activity edge-to-edge

--- app/src/main/java/fr/outadoc/quickhass/feature/onboarding/OnboardingActivity.kt
+++ app/src/main/java/fr/outadoc/quickhass/feature/onboarding/OnboardingActivity.kt
@@ -1,6 +1,9 @@
 package fr.outadoc.quickhass.feature.onboarding
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.navigation.Navigation.findNavController
 import fr.outadoc.quickhass.DayNightActivity
 import fr.outadoc.quickhass.R
@@ -16,6 +19,11 @@ class OnboardingActivity : DayNightActivity() {
         findNavController(this, R.id.nav_host_fragment)
             .navigate(R.id.welcomeFragment)
     }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet) =
+        super.onCreateView(name, context, attrs)?.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        }
 
     override fun onSupportNavigateUp(): Boolean {
         return findNavController(this, R.id.nav_host_fragment)
