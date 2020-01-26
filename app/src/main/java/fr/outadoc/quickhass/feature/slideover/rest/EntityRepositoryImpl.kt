54800a3d0f446b4c1cd03b09f7431094d3b0f8bc
Baptiste Candellier <outadoc@gmail.com>
2020-01-26 12:18:43 +0100

    feat(grid): improve tile sorting method

--- app/src/main/java/fr/outadoc/quickhass/feature/slideover/rest/EntityRepositoryImpl.kt
+++ app/src/main/java/fr/outadoc/quickhass/feature/slideover/rest/EntityRepositoryImpl.kt
@@ -6,8 +6,11 @@ import fr.outadoc.quickhass.feature.slideover.model.Tile
 import fr.outadoc.quickhass.model.Action
 import fr.outadoc.quickhass.model.EntityState
 import fr.outadoc.quickhass.model.Service
+import fr.outadoc.quickhass.model.entity.CoverEntity
 import fr.outadoc.quickhass.model.entity.Entity
 import fr.outadoc.quickhass.model.entity.EntityFactory
+import fr.outadoc.quickhass.model.entity.LightEntity
+import fr.outadoc.quickhass.model.entity.WeatherEntity
 import fr.outadoc.quickhass.persistence.EntityDatabase
 import fr.outadoc.quickhass.preferences.PreferenceRepository
 import fr.outadoc.quickhass.rest.AccessTokenProvider
@@ -23,7 +26,12 @@ class EntityRepositoryImpl(
 ) : EntityRepository {
 
     private val client: HomeAssistantApi by lazy {
-        RestClient.create<HomeAssistantApi>(loggingInterceptor, chuckerInterceptor, accessTokenProvider, prefs)
+        RestClient.create<HomeAssistantApi>(
+            loggingInterceptor,
+            chuckerInterceptor,
+            accessTokenProvider,
+            prefs
+        )
     }
 
     override suspend fun getEntityTiles(): Result<List<Tile<Entity>>> {
@@ -35,21 +43,46 @@ class EntityRepositoryImpl(
         return wrapResponse { client.getStates() }.map { states ->
             states.asSequence()
                 .map { EntityFactory.create(it) }
-                .sortedWith(
-                    compareBy(
-                        { persistedEntities[it.entityId]?.order ?: Int.MAX_VALUE },
-                        { it.domain })
-                )
                 .map { entity ->
                     val persistedEntity = persistedEntities[entity.entityId]
                     tileFactory
                         .create(entity)
                         .copy(
                             isHidden = persistedEntity?.hidden
-                                    ?: !entity.isVisible || INITIAL_DOMAIN_BLACKLIST.contains(entity.domain)
+                                ?: !entity.isVisible || INITIAL_DOMAIN_BLACKLIST.contains(entity.domain)
                         )
                 }
+                .sortedWith(
+                    compareBy(
+                        // If the user has already ordered the item manually, use that order
+                        // Otherwise put it at the end of the list initially
+                        { tile -> persistedEntities[tile.source.entityId]?.order ?: Int.MAX_VALUE },
+
+                        // Shove hidden tiles to the end of the list initially
+                        { tile -> tile.isHidden },
+
+                        // Order by domain priority (put lights and covers first for example)
+                        { tile ->
+                            getPriorityForDomain(tile.source.domain) ?: Int.MAX_VALUE
+                        },
+
+                        // Order by domain so that the items are somewhat sorted
+                        { tile -> tile.source.domain },
+
+                        // Order by label within a domain
+                        { tile -> tile.source.friendlyName }
+                    )
+                )
                 .toList()
+        }
+    }
+
+    private fun getPriorityForDomain(domain: String): Int? {
+        return when (domain) {
+            LightEntity.DOMAIN -> 0
+            CoverEntity.DOMAIN -> 1
+            WeatherEntity.DOMAIN -> 2
+            else -> null
         }
     }
 
