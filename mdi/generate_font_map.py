ebaf929fc5dcb2a2b2a02f8f60d8044f4c005e13
Baptiste Candellier <outadoc@gmail.com>
2020-01-29 14:43:50 +0000

    chore(icons): upgrade materialdesignicons version

--- mdi/generate_font_map.py
+++ mdi/generate_font_map.py
@@ -10,6 +10,9 @@ def get_script_path():
     return os.path.dirname(os.path.realpath(sys.argv[0]))
 
 
+out_path = get_script_path() + "/src/main/assets/mdi_map.txt"
+
+
 def main():
     parser = argparse.ArgumentParser(
         description='Generate the font map for Material Design Icons.')
@@ -17,19 +20,20 @@ def main():
                         help='the minified css file containing the mapping for the mdi font')
     args = parser.parse_args()
 
-    regex = r'\.mdi-([a-z\-0-9]+):before\{content:"\\([A-F0-9]{2,4})"\}'
+    regex = r'\.mdi-([a-z\-0-9]+)::before\{content:"\\([A-F0-9]{2,8})"\}'
 
     with open(args.input, 'r') as f_in:
         content = f_in.read()
 
-    with open(get_script_path() + "/src/main/assets/mdi_map.txt", 'w') as f_out:
+    with open(out_path, 'w') as f_out:
         for res in re.finditer(regex, content):
             name = res.group(1)
-            hex_char = res.group(2).ljust(4, "0")
+            hex_char = res.group(2).rjust(8, "0")
             char = bytes.fromhex(hex_char)
-            str_char = bytes.decode(char, 'utf-16-be')
+            str_char = bytes.decode(char, 'utf-32-be')
 
-            f_out.write(name + " " + str_char + "\n")
+            print(name + ' ' + str_char)
+            f_out.write(name + ' ' + str_char + "\n")
 
 
 if __name__ == "__main__":
