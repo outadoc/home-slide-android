3aea66517466b36c311d2540c6fda8a93110256c
Baptiste Candellier <outadoc@gmail.com>
2020-06-10 20:20:57 +0000

    fix(oauth): use homeslide:// for redirect url

--- app-wear/src/main/java/fr/outadoc/quickhass/wear/MainActivity.kt
+++ app-wear/src/main/java/fr/outadoc/quickhass/wear/MainActivity.kt
@@ -1,15 +1,9 @@
 package fr.outadoc.quickhass.wear
 
 import android.os.Bundle
-import com.github.ajalt.timberkt.Timber
-import com.google.android.gms.wearable.DataClient
-import com.google.android.gms.wearable.DataEventBuffer
-import com.google.android.gms.wearable.Wearable
 import fr.outadoc.homeslide.common.DayNightActivity
 import fr.outadoc.homeslide.common.ThemeProvider
 import fr.outadoc.quickhass.wear.feature.list.EntityGridFragment
-import fr.outadoc.quickhass.wear.preferences.PreferenceSyncViewModel
-import org.koin.android.ext.android.inject
 
 class MainActivity : DayNightActivity() {
 
