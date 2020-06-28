849445e6f91aa9319d048268ee83cd9602a07aac
Baptiste Candellier <outadoc@gmail.com>
2020-06-28 18:00:22 +0200

    feat(domain): media player support

--- lib-homeassistant-api/src/main/java/fr/outadoc/homeslide/hassapi/model/entity/MediaPlayer.kt
+++ lib-homeassistant-api/src/main/java/fr/outadoc/homeslide/hassapi/model/entity/MediaPlayer.kt
@@ -1,12 +1,29 @@
 package fr.outadoc.homeslide.hassapi.model.entity
 
+import fr.outadoc.homeslide.hassapi.model.Action
 import fr.outadoc.homeslide.hassapi.model.EntityState
 import fr.outadoc.homeslide.hassapi.model.entity.base.ToggleableEntity
+import fr.outadoc.mdi.FontIcon
 import fr.outadoc.mdi.toIcon
 
 class MediaPlayer(state: EntityState) : ToggleableEntity(state, "cast".toIcon()) {
 
     companion object {
         const val DOMAIN = "media_player"
+
+        private const val STATE_PLAYING = "playing"
+        private const val STATE_UNAVAILABLE = "unavailable"
     }
+
+    override val isOn: Boolean = stateStr == STATE_PLAYING
+
+    override val primaryAction =
+        Action(DOMAIN, "media_play_pause", entityId)
+
+    override val fallbackIcon: FontIcon?
+        get() = when (stateStr) {
+            STATE_PLAYING -> "cast-connected".toIcon()
+            STATE_UNAVAILABLE -> "cast-off".toIcon()
+            else -> null
+        }
 }
