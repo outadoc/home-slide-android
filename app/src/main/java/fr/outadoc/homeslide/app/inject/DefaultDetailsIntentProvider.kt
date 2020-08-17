8fe0132dda2f68be2422313f1f1a689e2b772332
Baptiste Candellier <outadoc@gmail.com>
2020-08-17 22:21:17 +0200

    Rename IntentProvider

--- app/src/main/java/fr/outadoc/homeslide/app/inject/DefaultDetailsIntentProvider.kt
+++ app/src/main/java/fr/outadoc/homeslide/app/inject/DefaultDetailsIntentProvider.kt
@@ -3,10 +3,10 @@ package fr.outadoc.homeslide.app.inject
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import fr.outadoc.homeslide.app.controlprovider.inject.IntentProvider
+import fr.outadoc.homeslide.app.controlprovider.inject.DetailsIntentProvider
 import fr.outadoc.homeslide.app.feature.slideover.BarebonesMainActivity
 
-class AppIntentProvider(private val context: Context) : IntentProvider {
+class DefaultDetailsIntentProvider(private val context: Context) : DetailsIntentProvider {
 
     override fun getEntityDetailsActivityIntent(): PendingIntent {
         val i = Intent(context, BarebonesMainActivity::class.java)
