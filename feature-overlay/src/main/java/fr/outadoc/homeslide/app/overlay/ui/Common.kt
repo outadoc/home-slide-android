6f5be700f1a14f0109312c5b447a559bf492b9cc
Baptiste Candellier <outadoc@gmail.com>
2020-09-03 19:59:26 +0200

    Setup compose

---
+++ feature-overlay/src/main/java/fr/outadoc/homeslide/app/overlay/ui/Common.kt
@@ -0,0 +1,9 @@
+package fr.outadoc.homeslide.app.overlay.ui
+
+import androidx.compose.runtime.Composable
+import androidx.ui.tooling.preview.Preview
+
+@Preview
+@Composable fun ControlOverlay() {
+
+}
