d8f54ac709506ad820bfbd70dd41bef959dc0a9a
Baptiste Candellier <outadoc@gmail.com>
2020-09-04 19:51:40 +0200

    Create some composables <3

--- lib-homeassistant-api/src/main/java/fr/outadoc/homeslide/hassapi/model/EntityState.kt
+++ lib-homeassistant-api/src/main/java/fr/outadoc/homeslide/hassapi/model/EntityState.kt
@@ -15,10 +15,6 @@ data class EntityState(
     @Json(name = "entity_id")
     @StringEntityId
     val entityId: String,
-    @Json(name = "last_changed")
-    val lastChanged: String,
-    @Json(name = "last_updated")
-    val lastUpdated: String,
     @Json(name = "state")
     @StringState
     val state: String,
