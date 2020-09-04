861fc31dc6dc1e07406041da3bcda410cf908ff0
Baptiste Candellier <outadoc@gmail.com>
2020-09-04 21:50:46 +0200

    Only keep slider

--- feature-overlay/src/main/java/fr/outadoc/homeslide/app/overlay/ui/Common.kt
+++ feature-overlay/src/main/java/fr/outadoc/homeslide/app/overlay/ui/Common.kt
@@ -1,8 +1,6 @@
 package fr.outadoc.homeslide.app.overlay.ui
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Slider
-import androidx.compose.material.Switch
 import androidx.compose.runtime.Composable
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.tooling.preview.PreviewParameter
@@ -10,7 +8,7 @@ import fr.outadoc.homeslide.hassapi.factory.EntityFactory
 import fr.outadoc.homeslide.hassapi.model.EntityState
 import fr.outadoc.homeslide.hassapi.model.entity.Light
 
-@Preview
+@Preview(name = "Control overlay")
 @Composable
 fun ControlOverlayPreview(
     @PreviewParameter(EntityStateParameterProvider::class)
@@ -28,15 +26,12 @@ fun ControlOverlay(entityState: EntityState) {
 
 @Composable
 fun LightControlOverlay(light: Light) {
-    Column {
-        Switch(checked = light.isOn, onCheckedChange = {
-            // toggle light
-        })
-
-        light.brightness?.let { brightness ->
-            Slider(value = brightness, onValueChange = {
+    light.brightness?.let { brightness ->
+        Slider(
+            value = brightness,
+            onValueChange = {
                 // update brightness
-            })
-        }
+            }
+        )
     }
 }
