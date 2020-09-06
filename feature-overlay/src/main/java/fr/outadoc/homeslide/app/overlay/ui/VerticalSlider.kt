2baac16d2e102793c7a8e43e2dd18eac9b0f91bc
Baptiste Candellier <outadoc@gmail.com>
2020-09-06 11:28:38 +0200

    Remove support for steps

--- feature-overlay/src/main/java/fr/outadoc/homeslide/app/overlay/ui/VerticalSlider.kt
+++ feature-overlay/src/main/java/fr/outadoc/homeslide/app/overlay/ui/VerticalSlider.kt
@@ -3,17 +3,12 @@ package fr.outadoc.homeslide.app.overlay.ui
 import androidx.compose.animation.asDisposableClock
 import androidx.compose.animation.core.AnimatedFloat
 import androidx.compose.animation.core.AnimationClockObservable
-import androidx.compose.animation.core.AnimationEndReason
-import androidx.compose.animation.core.TargetAnimation
 import androidx.compose.animation.core.TweenSpec
 import androidx.compose.foundation.Box
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Interaction
 import androidx.compose.foundation.InteractionState
 import androidx.compose.foundation.Strings
-import androidx.compose.foundation.animation.FlingConfig
-import androidx.compose.foundation.animation.defaultFlingConfig
-import androidx.compose.foundation.animation.fling
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.indication
 import androidx.compose.foundation.layout.Spacer
@@ -23,11 +18,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.preferredHeightIn
 import androidx.compose.foundation.layout.preferredSize
 import androidx.compose.foundation.layout.preferredWidthIn
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Slider
 import androidx.compose.material.SliderConstants.InactiveTrackColorAlpha
-import androidx.compose.material.SliderConstants.TickColorAlpha
 import androidx.compose.material.Surface
 import androidx.compose.material.ripple.RippleIndication
 import androidx.compose.runtime.Composable
@@ -36,11 +30,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.WithConstraints
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.lerp
 import androidx.compose.ui.gesture.pressIndicatorGestureFilter
 import androidx.compose.ui.gesture.scrollorientationlocking.Orientation
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.platform.AnimationClockAmbient
 import androidx.compose.ui.platform.DensityAmbient
@@ -52,11 +44,9 @@ import androidx.compose.ui.semantics.scrollForward
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.setProgress
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.util.annotation.IntRange
 import androidx.compose.ui.util.format
 import androidx.compose.ui.util.lerp
 import androidx.ui.tooling.preview.Preview
-import kotlin.math.abs
 import kotlin.math.roundToInt
 
 @Preview
@@ -64,8 +54,7 @@ import kotlin.math.roundToInt
 fun VerticalSliderPreview() {
     VerticalSlider(
         value = 0.5f,
-        onValueChange = {},
-        thumbColor = MaterialTheme.colors.primaryVariant
+        onValueChange = {}
     )
 }
 
@@ -75,43 +64,29 @@ fun VerticalSlider(
     onValueChange: (Float) -> Unit,
     modifier: Modifier = Modifier,
     valueRange: ClosedFloatingPointRange<Float> = 0f..1f,
-    @IntRange(from = 0) steps: Int = 0,
     onValueChangeEnd: () -> Unit = {},
-    thumbColor: Color = MaterialTheme.colors.primary,
+    thumbColor: Color = MaterialTheme.colors.primaryVariant,
     activeTrackColor: Color = MaterialTheme.colors.primary,
-    inactiveTrackColor: Color = activeTrackColor.copy(alpha = InactiveTrackColorAlpha),
-    activeTickColor: Color = MaterialTheme.colors.onPrimary.copy(alpha = TickColorAlpha),
-    inactiveTickColor: Color = activeTrackColor.copy(alpha = TickColorAlpha)
+    inactiveTrackColor: Color = activeTrackColor.copy(alpha = InactiveTrackColorAlpha)
 ) {
     val clock = AnimationClockAmbient.current.asDisposableClock()
-    val position = remember(valueRange, steps) {
+    val position = remember(valueRange) {
         SliderPosition(
             initial = value,
             valueRange = valueRange,
-            steps = steps,
             animatedClock = clock,
             onValueChange = onValueChange
         )
     }
     position.onValueChange = onValueChange
     position.scaledValue = value
-    WithConstraints(modifier.sliderSemantics(value, position, onValueChange, valueRange, steps)) {
+    WithConstraints(modifier.sliderSemantics(value, position, onValueChange, valueRange)) {
         val maxPx = constraints.maxHeight.toFloat()
         val minPx = 0f
         position.setBounds(minPx, maxPx)
 
-        val flingConfig = SliderFlingConfig(position, position.anchorsPx)
-        val gestureEndAction = { velocity: Float ->
-            if (flingConfig != null) {
-                position.holder.fling(velocity, flingConfig) { reason, endValue, _ ->
-                    if (reason != AnimationEndReason.Interrupted) {
-                        position.holder.snapTo(endValue)
-                        onValueChangeEnd()
-                    }
-                }
-            } else {
-                onValueChangeEnd()
-            }
+        val gestureEndAction = { _: Float ->
+            onValueChangeEnd()
         }
 
         val interactionState = remember { InteractionState() }
@@ -141,12 +116,9 @@ fun VerticalSlider(
         val fraction = calcFraction(position.startValue, position.endValue, coerced)
         SliderImpl(
             positionFraction = fraction,
-            tickFractions = position.tickFractions,
             thumbColor = thumbColor,
             trackColor = activeTrackColor,
             inactiveTrackColor = inactiveTrackColor,
-            activeTickColor = activeTickColor,
-            inactiveTickColor = inactiveTickColor,
             height = maxPx,
             interactionState = interactionState,
             modifier = press.then(drag)
@@ -157,12 +129,9 @@ fun VerticalSlider(
 @Composable
 private fun SliderImpl(
     positionFraction: Float,
-    tickFractions: List<Float>,
     thumbColor: Color,
     trackColor: Color,
     inactiveTrackColor: Color,
-    activeTickColor: Color,
-    inactiveTickColor: Color,
     height: Float,
     interactionState: InteractionState,
     modifier: Modifier
@@ -185,10 +154,7 @@ private fun SliderImpl(
             modifier = center.fillMaxSize(),
             color = trackColor,
             inactiveColor = inactiveTrackColor,
-            activeTickColor = activeTickColor,
-            inactiveTickColor = inactiveTickColor,
             positionFraction = positionFraction,
-            tickFractions = tickFractions,
             thumbPx = thumbPx,
             trackStrokeWidth = trackStrokeWidth
         )
@@ -201,7 +167,7 @@ private fun SliderImpl(
                 ThumbDefaultElevation
             }
             Surface(
-                shape = CircleShape,
+                shape = RoundedCornerShape(8.dp),
                 color = thumbColor,
                 elevation = elevation,
                 modifier = Modifier.indication(
@@ -223,46 +189,36 @@ private fun Track(
     modifier: Modifier,
     color: Color,
     inactiveColor: Color,
-    activeTickColor: Color,
-    inactiveTickColor: Color,
     positionFraction: Float,
-    tickFractions: List<Float>,
     thumbPx: Float,
     trackStrokeWidth: Float
 ) {
     Canvas(modifier) {
         val sliderStart = Offset(x = center.x, y = size.height - thumbPx)
         val sliderEnd = Offset(x = center.x, y = thumbPx)
+
+        // Main track background
         drawLine(
             color = inactiveColor,
             start = sliderStart,
             end = sliderEnd,
             strokeWidth = trackStrokeWidth,
-            cap = StrokeCap.Round
+            cap = StrokeCap.Square
         )
+
         val sliderValue = Offset(
             x = center.x,
             y = sliderStart.y - (sliderEnd.y + sliderStart.y) * positionFraction
         )
 
+        // Active track background
         drawLine(
             color = color,
             start = sliderStart,
             end = sliderValue,
             strokeWidth = trackStrokeWidth,
-            cap = StrokeCap.Round
+            cap = StrokeCap.Square
         )
-        tickFractions.groupBy { it > positionFraction }.forEach { (afterFraction, list) ->
-            drawPoints(
-                list.map {
-                    Offset(lerp(sliderStart, sliderEnd, it).x, center.y)
-                },
-                PointMode.Points,
-                if (afterFraction) inactiveTickColor else activeTickColor,
-                trackStrokeWidth,
-                StrokeCap.Round
-            )
-        }
     }
 }
 
@@ -274,30 +230,11 @@ private fun scale(a1: Float, b1: Float, x1: Float, a2: Float, b2: Float) =
 private fun calcFraction(a: Float, b: Float, pos: Float) =
     (if (b - a == 0f) 0f else (pos - a) / (b - a)).coerceIn(0f, 1f)
 
-@Composable
-private fun SliderFlingConfig(
-    value: SliderPosition,
-    anchors: List<Float>
-): FlingConfig? {
-    return if (anchors.isEmpty()) {
-        null
-    } else {
-        val adjustTarget: (Float) -> TargetAnimation? = { _ ->
-            val now = value.holder.value
-            val point = anchors.minByOrNull { abs(it - now) }
-            val adjusted = point ?: now
-            TargetAnimation(adjusted, SliderToTickAnimation)
-        }
-        defaultFlingConfig(adjustTarget = adjustTarget)
-    }
-}
-
 private fun Modifier.sliderSemantics(
     value: Float,
     position: SliderPosition,
     onValueChange: (Float) -> Unit,
-    valueRange: ClosedFloatingPointRange<Float> = 0f..1f,
-    @IntRange(from = 0) steps: Int = 0
+    valueRange: ClosedFloatingPointRange<Float> = 0f..1f
 ): Modifier {
     val coerced = value.coerceIn(position.startValue, position.endValue)
     val fraction = calcFraction(position.startValue, position.endValue, coerced)
@@ -310,24 +247,21 @@ private fun Modifier.sliderSemantics(
     return semantics {
         accessibilityValue = Strings.TemplatePercent.format(percent)
         accessibilityValueRange = AccessibilityRangeInfo(coerced, valueRange)
-        setProgress(action = { setSliderProgress(it, coerced, position, onValueChange, steps) })
+        setProgress(action = { setSliderProgress(it, coerced, position, onValueChange) })
 
         // TODO(b/157692376) Remove accessibility scroll actions in Slider when
         //  talkback is fixed
-        var increment = (position.endValue - position.startValue) / AccessibilityStepsCount
-        if (steps > 0) {
-            increment = (position.endValue - position.startValue) / (steps + 1)
-        }
+        val increment = (position.endValue - position.startValue) / AccessibilityStepsCount
         if (coerced < position.endValue) {
             @Suppress("DEPRECATION")
             scrollForward(action = {
-                setSliderProgress(coerced + increment, coerced, position, onValueChange, steps)
+                setSliderProgress(coerced + increment, coerced, position, onValueChange)
             })
         }
         if (coerced > position.startValue) {
             @Suppress("DEPRECATION")
             scrollBackward(action = {
-                setSliderProgress(coerced - increment, coerced, position, onValueChange, steps)
+                setSliderProgress(coerced - increment, coerced, position, onValueChange)
             })
         }
     }
@@ -337,17 +271,9 @@ private fun setSliderProgress(
     targetValue: Float,
     currentValue: Float,
     position: SliderPosition,
-    onValueChange: (Float) -> Unit,
-    @IntRange(from = 0) steps: Int = 0
+    onValueChange: (Float) -> Unit
 ): Boolean {
-    var newValue = targetValue.coerceIn(position.startValue, position.endValue)
-    if (steps >= 0) {
-        val anchorsValue = position.tickFractions.map {
-            lerp(position.startValue, position.endValue, it)
-        }
-        val point = anchorsValue.minByOrNull { abs(it - newValue) }
-        newValue = point ?: newValue
-    }
+    val newValue = targetValue.coerceIn(position.startValue, position.endValue)
     // This is to keep it consistent with AbsSeekbar.java: return false if no
     // change from current.
     if (newValue == currentValue) {
@@ -374,19 +300,12 @@ private const val AccessibilityStepsCount = 20
 private class SliderPosition(
     initial: Float = 0f,
     val valueRange: ClosedFloatingPointRange<Float> = 0f..1f,
-    @IntRange(from = 0) steps: Int = 0,
     animatedClock: AnimationClockObservable,
     var onValueChange: (Float) -> Unit
 ) {
 
     val startValue: Float = valueRange.start
     val endValue: Float = valueRange.endInclusive
-
-    init {
-        require(steps >= 0) {
-            "steps should be >= 0"
-        }
-    }
 
     var scaledValue: Float = initial
         set(value) {
@@ -408,17 +327,8 @@ private class SliderPosition(
         startPx = min
         endPx = max
         holder.setBounds(min, max)
-        anchorsPx = tickFractions.map {
-            lerp(startPx, endPx, it)
-        }
         holder.snapTo(newValue)
     }
-
-    val tickFractions: List<Float> =
-        if (steps == 0) emptyList() else List(steps + 2) { it.toFloat() / (steps + 1) }
-
-    var anchorsPx: List<Float> = emptyList()
-        private set
 
     @Suppress("UnnecessaryLambdaCreation")
     val holder =
@@ -454,5 +364,3 @@ private val SliderMinHeight = 96.dp
 private val DefaultSliderConstraints =
     Modifier.preferredHeightIn(minHeight = SliderMinHeight)
         .preferredWidthIn(maxWidth = SliderWidth)
-
-private val SliderToTickAnimation = TweenSpec<Float>(durationMillis = 100)
