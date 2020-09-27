956d9396328a6efd68137c17d6e94b2008df5167
Baptiste Candellier <outadoc@gmail.com>
2020-09-27 16:37:17 +0000

    chore(mdi): improve parsing perfs, bump version

--- lib-mdi/src/main/java/fr/outadoc/mdi/MaterialIconAssetMapperImpl.kt
+++ lib-mdi/src/main/java/fr/outadoc/mdi/MaterialIconAssetMapperImpl.kt
@@ -3,43 +3,41 @@ package fr.outadoc.mdi
 import android.content.Context
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
+import java.util.TreeMap
 
 /**
  * Maps an icon name to a unicode codepoint that can be displayed using the MDI font.
  */
-class MaterialIconAssetMapperImpl(private val context: Context) : MaterialIconMapper {
+class MaterialIconAssetMapperImpl(context: Context) : MaterialIconMapper {
 
     companion object {
-        const val FILENAME = "mdi_map.txt"
-        const val SEPARATOR = ' '
-        const val COMMENT = '#'
+        private const val FILENAME = "mdi_map.txt"
+        private const val SEPARATOR = ' '
+    }
+
+    private val iconMap: Map<String, String>
+
+    init {
+        context.assets.open(FILENAME).use { file ->
+            iconMap = BufferedReader(
+                InputStreamReader(file, StandardCharsets.UTF_8)
+            ).use { reader ->
+                loadMap(reader)
+            }
+        }
     }
 
     override fun getIcon(@IconStringRef iconName: String): FontIcon? {
-        val icon = iconMap[iconName]
-        return icon?.let { FontIcon(it) }
+        return iconMap[iconName]?.let { FontIcon(it) }
     }
 
     private fun loadMap(reader: BufferedReader): Map<String, String> {
-        val map = mutableMapOf<String, String>()
-
+        val map = TreeMap<String, String>()
         reader.forEachLine { line ->
-            if (line.startsWith(COMMENT) || line.isBlank())
-                return@forEachLine
-
-            val id = line.takeWhile { it != SEPARATOR }
-            val char = line.takeLastWhile { it != SEPARATOR }
-            map[id] = char
+            val split = line.split(SEPARATOR, limit = 2)
+            map[split[0]] = split[1]
         }
-
         return map
-    }
-
-    private val iconMap: Map<String, String> by lazy {
-        val file = context.assets.open(FILENAME)
-        BufferedReader(InputStreamReader(file, Charset.forName("utf-8"))).use { reader ->
-            loadMap(reader)
-        }
     }
 }
