668fe71cf9c74682d4eac03b74c928791e03bb1b
Baptiste Candellier <outadoc@gmail.com>
2020-11-26 22:25:13 +0100

    fix(wear): fix about background on wear os

--- app-wear/src/main/java/fr/outadoc/homeslide/wear/feature/about/AboutFragment.kt
+++ app-wear/src/main/java/fr/outadoc/homeslide/wear/feature/about/AboutFragment.kt
@@ -21,10 +21,14 @@
 package fr.outadoc.homeslide.wear.feature.about
 
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
 import android.preference.Preference
 import android.preference.PreferenceFragment
 import android.preference.SwitchPreference
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import fr.outadoc.homeslide.common.feature.about.ThirdPartyLibraries
 import fr.outadoc.homeslide.logging.KLog
@@ -37,7 +41,8 @@ class AboutFragment : PreferenceFragment() {
         addPreferencesFromResource(R.xml.preferences_about)
 
         val versionPref: Preference? = findPreference("pref_about_version")
-        val enableCrashReportingPref = findPreference("chk_pref_enable_crash_reporting") as SwitchPreference?
+        val enableCrashReportingPref =
+            findPreference("chk_pref_enable_crash_reporting") as SwitchPreference?
 
         versionPref?.summary = try {
             val pInfo =
@@ -60,6 +65,14 @@ class AboutFragment : PreferenceFragment() {
                     .joinToString(separator = "\n")
             }
         }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater?,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = super.onCreateView(inflater, container, savedInstanceState)?.apply {
+        setBackgroundColor(Color.BLACK)
     }
 
     private fun requireContext() = requireNotNull(context)
