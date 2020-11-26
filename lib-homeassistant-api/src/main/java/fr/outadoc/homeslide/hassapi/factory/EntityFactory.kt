edb60819f17678cc379d556f6440f36401b2d149
Baptiste Candellier <outadoc@gmail.com>
2020-11-26 23:00:10 +0100

    feat(domain): add support for scene domain

--- lib-homeassistant-api/src/main/java/fr/outadoc/homeslide/hassapi/factory/EntityFactory.kt
+++ lib-homeassistant-api/src/main/java/fr/outadoc/homeslide/hassapi/factory/EntityFactory.kt
@@ -31,6 +31,7 @@ import fr.outadoc.homeslide.hassapi.model.entity.Light
 import fr.outadoc.homeslide.hassapi.model.entity.Lock
 import fr.outadoc.homeslide.hassapi.model.entity.MediaPlayer
 import fr.outadoc.homeslide.hassapi.model.entity.Person
+import fr.outadoc.homeslide.hassapi.model.entity.Scene
 import fr.outadoc.homeslide.hassapi.model.entity.Script
 import fr.outadoc.homeslide.hassapi.model.entity.Sensor
 import fr.outadoc.homeslide.hassapi.model.entity.Sun
@@ -41,25 +42,26 @@ import fr.outadoc.homeslide.hassapi.model.entity.Weather
 object EntityFactory {
 
     fun create(state: EntityState) = when (state.domain) {
-        Light.DOMAIN -> Light(state)
-        Cover.DOMAIN -> Cover(state)
-        Person.DOMAIN -> Person(state)
-        Sun.DOMAIN -> Sun(state)
-        Switch.DOMAIN -> Switch(state)
-        Sensor.DOMAIN -> Sensor(state)
-        Script.DOMAIN -> Script(state)
         Automation.DOMAIN -> Automation(state)
-        Group.DOMAIN -> Group(state)
+        BinarySensor.DOMAIN -> BinarySensor(state)
+        Calendar.DOMAIN -> Calendar(state)
         Climate.DOMAIN -> Climate(state)
-        MediaPlayer.DOMAIN -> MediaPlayer(state)
-        Weather.DOMAIN -> Weather(state)
+        Cover.DOMAIN -> Cover(state)
+        Fan.DOMAIN -> Fan(state)
+        Group.DOMAIN -> Group(state)
         InputBoolean.DOMAIN -> InputBoolean(state)
         InputNumber.DOMAIN -> InputNumber(state)
-        BinarySensor.DOMAIN -> BinarySensor(state)
-        Vacuum.DOMAIN -> Vacuum(state)
+        Light.DOMAIN -> Light(state)
         Lock.DOMAIN -> Lock(state)
-        Calendar.DOMAIN -> Calendar(state)
-        Fan.DOMAIN -> Fan(state)
+        MediaPlayer.DOMAIN -> MediaPlayer(state)
+        Person.DOMAIN -> Person(state)
+        Scene.DOMAIN -> Scene(state)
+        Script.DOMAIN -> Script(state)
+        Sensor.DOMAIN -> Sensor(state)
+        Sun.DOMAIN -> Sun(state)
+        Switch.DOMAIN -> Switch(state)
+        Vacuum.DOMAIN -> Vacuum(state)
+        Weather.DOMAIN -> Weather(state)
         else -> GenericEntity(state)
     }
 }
