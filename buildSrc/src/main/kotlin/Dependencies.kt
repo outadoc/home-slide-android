a04ec5d987bce72ad6a275b7ac5d8e3aa3d6981d
Baptiste Candellier <outadoc@gmail.com>
2021-02-09 19:58:27 +0100

    Update Material

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -72,7 +72,7 @@ object Dependencies {
     }
 
     object Google {
-        const val material = "com.google.android.material:material:1.3.0-alpha04"
+        const val material = "com.google.android.material:material:1.3.0"
 
         object Firebase {
             object Crashlytics {
