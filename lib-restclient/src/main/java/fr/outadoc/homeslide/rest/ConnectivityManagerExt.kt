5d441c71af7451e5ba3761912af4aca77932dcd5
Baptiste Candellier <outadoc@gmail.com>
2021-02-21 14:25:16 +0100

    Run spotless

--- lib-restclient/src/main/java/fr/outadoc/homeslide/rest/ConnectivityManagerExt.kt
+++ lib-restclient/src/main/java/fr/outadoc/homeslide/rest/ConnectivityManagerExt.kt
@@ -20,9 +20,9 @@ import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkRequest
 import android.os.Build
+import kotlin.coroutines.resume
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withTimeout
-import kotlin.coroutines.resume
 
 suspend fun ConnectivityManager.requestNetwork(request: NetworkRequest, timeoutMs: Int): Network? =
     withTimeout(timeoutMs.toLong()) {
