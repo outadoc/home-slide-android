ebc04fd5f3a34981b6b2eea04e18bf2c58c2b45d
Baptiste Candellier <outadoc@gmail.com>
2021-03-20 13:23:19 +0100

    release(1.6.5)

--- buildSrc/src/main/kotlin/AppInfo.kt
+++ buildSrc/src/main/kotlin/AppInfo.kt
@@ -17,8 +17,8 @@
 @Suppress("MemberVisibilityCanBePrivate")
 object AppInfo {
 
-    const val versionName = "1.6.4"
-    const val baseVersionCode = 166
+    const val versionName = "1.6.5"
+    const val baseVersionCode = 167
 
     const val targetSdkVersion = 30
 
