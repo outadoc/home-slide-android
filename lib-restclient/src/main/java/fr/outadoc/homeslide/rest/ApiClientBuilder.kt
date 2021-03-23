1087ca2a9e4a355c44063364a9bd9b3366646e36
Baptiste Candellier <outadoc@gmail.com>
2021-03-23 22:34:07 +0100

    Apply spotless

--- lib-restclient/src/main/java/fr/outadoc/homeslide/rest/ApiClientBuilder.kt
+++ lib-restclient/src/main/java/fr/outadoc/homeslide/rest/ApiClientBuilder.kt
@@ -27,13 +27,12 @@ import fr.outadoc.homeslide.rest.tls.UnsafeX509TrustManager
 import fr.outadoc.homeslide.rest.tls.createSocketFactory
 import fr.outadoc.homeslide.rest.tls.defaultTrustManager
 import fr.outadoc.homeslide.rest.util.PLACEHOLDER_BASE_URL
+import java.util.concurrent.TimeUnit
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.internal.Util
 import okhttp3.internal.tls.OkHostnameVerifier
 import retrofit2.Converter
 import retrofit2.Retrofit
-import java.util.concurrent.TimeUnit
 
 class ApiClientBuilder<T>(
     private val type: Class<T>,
